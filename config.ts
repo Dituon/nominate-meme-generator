71718f6dedef6b83f72f55985f698b24dcc020ce
Dituon <68615161+Dituon@users.noreply.github.com>
2024-02-13 03:42:00 +0900

    fix(Config): baseUrl

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const config = {
   repoLoader: {
-    // baseUrl: 'https://d2n.moe/nominate-meme-data',
-    baseUrl: 'http://127.0.0.1:5500',
+    baseUrl: 'https://d2n.moe/nominate-meme-data',
+    // baseUrl: 'http://127.0.0.1:5500',
   },
 }
 
